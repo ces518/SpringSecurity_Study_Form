83eb929acea4640dc1938debc5275bc7f8e72dfe
ces518 <pupupee9@gmail.com>
2019-09-05 21:52:34 +0900

    Spring Security - Project Init / 각 페이지 구현

---
+++ src/main/java/me/june/springsecurity/form/SampleController.java
@@ -0,0 +1,45 @@
+package me.june.springsecurity.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 05/09/2019
+ * Time: 9:35 오후
+ **/
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index (Model model, Principal principal) {
+        if (principal == null) {
+            model.addAttribute("message", "Hello Spring Security");
+        } else {
+            model.addAttribute("message", "Hello Index" + principal.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info (Model model) {
+        model.addAttribute("message", "Hello Info");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard (Model model, Principal principal) {
+        model.addAttribute("message", "Hello " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin (Model model, Principal principal) {
+        model.addAttribute("message", "Hello Admin" + principal.getName());
+        return "admin";
+    }
+}
