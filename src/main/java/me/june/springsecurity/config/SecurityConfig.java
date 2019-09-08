98b348fb009268c17e0e27aa830d08ed9e8c454b
ces518 <pupupee9@gmail.com>
2019-09-08 14:34:07 +0900

    Spring Security - Security Config / 시큐리티 기본 적용

---
+++ src/main/java/me/june/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package me.june.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 07/09/2019
+ * Time: 10:29 오후
+ **/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .and()
+            .httpBasic();
+    }
+}
