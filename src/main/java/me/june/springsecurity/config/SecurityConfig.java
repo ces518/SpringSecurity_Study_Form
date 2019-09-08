fcf9fd794f0ac5395284b72c143e60440f8b5f0e
ces518 <pupupee9@gmail.com>
2019-09-08 15:05:40 +0900

    Spring Security - Security Config / InMemoryUser 설정

--- src/main/java/me/june/springsecurity/config/SecurityConfig.java
+++ src/main/java/me/june/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.june.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,5 +26,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
                 .and()
             .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}1234").roles("ADMIN").and()
+                .withUser("user").password("{noop}user1234").roles("USER");
     }
 }
