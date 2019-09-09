af9ee8c4d260283023790a382094789563ae5959
ces518 <pupupee9@gmail.com>
2019-09-09 23:32:38 +0900

    Spring Security - Security Test / formLogin 테스트

--- src/test/java/me/june/springsecurity/account/AccountControllerTest.java
+++ src/test/java/me/june/springsecurity/account/AccountControllerTest.java
@@ -9,8 +9,12 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,6 +27,9 @@ public class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     @Test
     @WithAnonymousUser
@@ -62,5 +69,48 @@ public class AccountControllerTest {
                     .with(user("admin").roles("ADMIN")))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+
+    // 테스트코드에서 데이터베이스에 변경이 일어나는 경우 Transactional 을 사용하는것이 좋다.
+    // 테스트가 끝난뒤에 롤백된다.
+    // 다른 테스트에 영향을 주지않는 독립적인 테스트이다.
+    // 다른 테스트에 영향을 주면 좋지 않은 테스트이다.
+    // javax, spring 모두 사용가능
+    // spring 도 java 표준을 지키고 있으며 spring 이 제공하는 기능이 좀더 많음
+    @Test
+    @Transactional
+    public void login () throws Exception {
+        // given
+        final String username = "june";
+        final String password = "1234";
+        Account createdAccount = createUser(username, password);
+
+        //when & then
+        mockMvc.perform(formLogin().user(username).password(password))
+            .andExpect(authenticated());
+    }
+
+
+    @Test
+    @Transactional
+    public void bad_credentials () throws Exception {
+        // given
+        final String username = "june";
+        final String password = "1234";
+        Account createdAccount = createUser(username, password);
+
+        //when & then
+        mockMvc.perform(formLogin().user(username).password("!2345678"))
+                .andExpect(unauthenticated());
+    }
+
+
+    private Account createUser(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+        return accountService.createAccount(account);
     }
 }
