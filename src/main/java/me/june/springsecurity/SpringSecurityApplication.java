39a4fa130f600ae1e410a4de5b71093e7b7aa580
ces518 <pupupee9@gmail.com>
2019-09-24 23:08:27 +0900

    Spring Security - @EnableAsync

--- src/main/java/me/june/springsecurity/SpringSecurityApplication.java
+++ src/main/java/me/june/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,11 @@ package me.june.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync // Async 한 기능을 활성화
+// AsyncThreadPool 설정도 해주어야하지만 기본 스레드풀을 사용한다.
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
